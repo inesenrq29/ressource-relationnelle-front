24654cbcab947d41782461535b69c4e032692ba6
Sarah Testelin <sarah.khelil0709@gmail.com>
2026-04-24 17:12:24 +0200

    fix on register after MR on required

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -46,33 +46,20 @@ export class RegisterComponent {
   );
 
   submit(): void {
-    console.log('submit appelé');
-    console.log('valid', this.form.valid);
-    console.log('form errors', this.form.errors);
-    console.log('value', this.form.getRawValue());
-
-    Object.entries(this.form.controls).forEach(([name, control]) => {
-      console.log(name, control.value, control.valid, control.errors);
-    });
-
     if (this.form.invalid) {
       this.form.markAllAsTouched();
-      console.log('bloqué car invalide');
       return;
     }
 
     this.loading = true;
     this.errorMessage = '';
 
-    console.log('appel API');
     this.authService.register(this.form.getRawValue()).subscribe({
-      next: (res) => {
-        console.log('OK', res);
+      next: () => {
         this.loading = false;
         this.router.navigateByUrl('/home');
       },
       error: (err) => {
-        console.error('ERR', err);
         this.loading = false;
         this.errorMessage = this.authService.getErrorMessage(err);
       },
